5d4a7d0ef289bc572860c08f321ecf186c602546
Kyriakos Kyriakou <kyriakouks@csd.auth.gr>
2024-05-19 13:01:58 +0300

    added js functions + made the search results hidden

--- products.js
+++ products.js
@@ -8,12 +8,22 @@ window.onload = () => {
 
 searchButtonOnClick = () => {
     // BEGIN CODE HERE
-
+    var x = document.getElementById("hiddenDiv");
+    const input = document.getElementById("searchProduct").value;
+    
+    if (input != ""){
+        x.style.display = "block"; //shows the results-table only when we search
+    }
     // END CODE HERE
 }
 
 productFormOnSubmit = (event) => {
     // BEGIN CODE HERE
+    const name = document.getElementById("product-name").value;
+    const year = document.getElementById("product-year").value;
+    const price = document.getElementById("product-price").value;
+    const color = document.getElementById("product-color").value;
+    const size = document.getElementById("product-size").value;
 
     // END CODE HERE
 }
